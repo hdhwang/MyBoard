b459ed899ff8f9cbabd26888fd96b88f1f1ecdc8
HDHWANG-DESKTOP\hadon <hadong0624@naver.com>
2022-07-11 23:22:02 +0900

    게시글 모델, 시리얼라이저, 뷰(CRUD) + 권한 구현

---
+++ posts/models.py
@@ -0,0 +1,16 @@
+from tkinter import CASCADE
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from users.models import Profile
+
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, blank=True)
+    title = models.CharField(max_length=128)
+    category = models.CharField(max_length=128)
+    body = models.TextField()
+    image = models.ImageField(upload_to='post/', default='default.png')
+    likes = models.ManyToManyField(User, related_name='like_posts', blank=True)
+    published_date = models.DateTimeField(default=timezone.now)
