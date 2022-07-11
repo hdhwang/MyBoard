49f5077de330ed68fa4d1d8f67021dee613254f9
HDHWANG-DESKTOP\hadon <hadong0624@naver.com>
2022-07-12 00:42:36 +0900

    예외 응답 포맷 변경

--- config/settings.py
+++ config/settings.py
@@ -56,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'config.custom_exception_handler.ExceptionMiddleware',
 ]
 
 # CORS_ORIGIN_ALLOW_ALL = True
@@ -125,6 +126,7 @@ REST_FRAMEWORK = {
         'django_filters.rest_framework.DjangoFilterBackend',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination', 'PAGE_SIZE': 3,
+    'EXCEPTION_HANDLER': 'config.custom_exception_handler.handle_exception',
 }
 
 
